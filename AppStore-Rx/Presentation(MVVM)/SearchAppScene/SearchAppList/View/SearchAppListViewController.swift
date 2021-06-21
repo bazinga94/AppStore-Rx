738a29835f883a7da900f8e6c92f7deb917ea14e
이종호 <bazinga940@gmail.com>
2021-06-21 17:04:34 +0900

    Launch 후 시작 view controller 세팅
    storyboard 대신 xib 사용

---
+++ AppStore-Rx/Presentation(MVVM)/SearchAppScene/SearchAppList/View/SearchAppListViewController.swift
@@ -0,0 +1,16 @@
+//
+//  SearchAppListViewController.swift
+//  AppStore-Rx
+//
+//  Created by Jongho Lee on 2021/06/21.
+//
+
+import UIKit
+
+class SearchAppListViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+	}
+}
