37d3f7873db770d008e7508b37c5bf60f165a728
Jongho Lee <bazinga940@gmail.com>
2021-06-22 23:20:25 +0900

    빈파일 생성

---
+++ AppStore-Rx/Data(Repository)/Repository/SearchAppListRepository.swift
@@ -0,0 +1,8 @@
+//
+//  SearchAppListRepository.swift
+//  AppStore-Rx
+//
+//  Created by Jongho Lee on 2021/06/22.
+//
+
+import Foundation
---
+++ AppStore-Rx/Domain(Business)/UseCase/SearchAppListUseCase.swift
@@ -0,0 +1,8 @@
+//
+//  SearchAppListUseCase.swift
+//  AppStore-Rx
+//
+//  Created by Jongho Lee on 2021/06/22.
+//
+
+import Foundation
