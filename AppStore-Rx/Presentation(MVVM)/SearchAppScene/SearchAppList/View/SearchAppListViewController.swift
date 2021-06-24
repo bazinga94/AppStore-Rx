11fecd4365f881f3a63463fc38efb02a1a4d1638
이종호 <bazinga940@gmail.com>
2021-06-24 10:35:03 +0900

    UITableView 대신 UIImageView에 사진을 띄울 수 있게 구현

--- AppStore-Rx/Presentation(MVVM)/SearchAppScene/SearchAppList/View/SearchAppListViewController.swift
+++ AppStore-Rx/Presentation(MVVM)/SearchAppScene/SearchAppList/View/SearchAppListViewController.swift
@@ -13,15 +13,14 @@ class SearchAppListViewController: UIViewController, StoryboardInstantiable {
 
 	@IBOutlet weak var searchBar: UISearchBar!
 	@IBOutlet weak var tableView: UITableView!
+	@IBOutlet weak var imageView: UIImageView!
 
 	private var viewModel: SearchAppListViewModel!
 	private var bag = DisposeBag()
-//	private var appInfoListRepository: AppInfoListRepository?
 
 	static func create(with viewModel: SearchAppListViewModel) -> SearchAppListViewController {
 		let vc = SearchAppListViewController.instantiateViewController()
 		vc.viewModel = viewModel
-//		vc.appInfoListRepository = appInfoListRepository
 		return vc
 	}
 
@@ -29,10 +28,12 @@ class SearchAppListViewController: UIViewController, StoryboardInstantiable {
 		super.viewDidLoad()
 		viewModel.viewDidLoad()
 		configureSearchBar()
-		self.viewModel.appInfoListObservable.bind(to: tableView.rx.items(cellIdentifier: SearchAppListTableViewCell.className, cellType: SearchAppListTableViewCell.self)) { row, element, cell in
-			cell.iconImageView.load(url: element.appIconImageUrl)
-		}
-		.disposed(by: bag)
+
+//		self.tableView.rx.setDelegate(self).disposed(by: bag)
+//		self.viewModel.appInfoListObservable.bind(to: tableView.rx.items(cellIdentifier: SearchAppListTableViewCell.className, cellType: SearchAppListTableViewCell.self)) { row, element, cell in
+//			cell.iconImageView.load(url: element.appIconImageUrl)
+//		}
+//		.disposed(by: bag)
 	}
 
 	/// searchBar 속성 구성
@@ -51,5 +52,16 @@ extension SearchAppListViewController: UISearchBarDelegate {
 		guard let searchText = searchBar.text?.trimmingCharacters(in: .whitespacesAndNewlines) else { return }
 		if searchText.count == 0 { return }
 		viewModel.didSearch(query: searchText)
+			.observe(on: MainScheduler.instance)
+			.subscribe(onNext: { [weak self] appInfoList in //, onError: <#T##((Error) -> Void)?##((Error) -> Void)?##(Error) -> Void#>, onCompleted: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>, onDisposed: <#T##(() -> Void)?##(() -> Void)?##() -> Void#>)
+				self?.imageView.load(url: appInfoList[0].appIconImageUrl)
+			})
+			.disposed(by: bag)
+	}
+}
+
+extension SearchAppListViewController: UITableViewDelegate {
+	func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+		return 200
 	}
 }
--- AppStore-Rx/Presentation(MVVM)/SearchAppScene/SearchAppList/ViewModel/SearchAppListViewModel.swift
+++ AppStore-Rx/Presentation(MVVM)/SearchAppScene/SearchAppList/ViewModel/SearchAppListViewModel.swift
@@ -11,7 +11,7 @@ import RxCocoa
 
 protocol SearchAppListViewModelInput {
 	func viewDidLoad()
-	func didSearch(query: String)
+	func didSearch(query: String) -> Observable<[AppInfo]>
 }
 
 protocol SearchAppListViewModelOutput {
@@ -25,21 +25,39 @@ class SearchAppListViewModel: SearchAppListViewModelInput, SearchAppListViewMode
 
 	init(searchAppListUseCase: SearchAppListUseCase) {
 		self.searchAppListUseCase = searchAppListUseCase
-		self.appInfoListObservable = Observable.of()
+		self.appInfoListObservable = Observable.of([AppInfo(appIconImageUrl: "https://is1-ssl.mzstatic.com/image/thumb/Purple115/v4/e8/d9/e9/e8d9e924-852b-cb7b-3852-432ef6941ae3/source/512x512bb.jpg", appName: "WorldBox - 샌드 박스 새로운 시뮬레이터", appGenre: "게임", numberOfReviews: "375", firstScreenShotUrl: "https://is4-ssl.mzstatic.com/image/thumb/Purple113/v4/c3/08/8f/c3088fad-ade5-e39f-93c6-129d7f2817b1/pr_source.png/392x696bb.png", secondScreenShotUrl: "https://is4-ssl.mzstatic.com/image/thumb/Purple113/v4/c3/08/8f/c3088fad-ade5-e39f-93c6-129d7f2817b1/pr_source.png/392x696bb.png", thirdScreenShotUrl: "https://is4-ssl.mzstatic.com/image/thumb/Purple113/v4/c3/08/8f/c3088fad-ade5-e39f-93c6-129d7f2817b1/pr_source.png/392x696bb.png", averageUserRating: 4.655989999999998)])
 	}
 
 	func viewDidLoad() {
 	}
 
-	func didSearch(query: String) {
-		let requestModel = SearchAppListUseCaseRequestModel(query: query)
-		searchAppLoadTask = searchAppListUseCase.execute(requestModel: requestModel) { [weak self] (result: Result<AppInfoList, Error>) in
-			switch result {
-				case .success(let model):
-					self?.appInfoListObservable = Observable.of(model.displayedApps)
-				case .failure(let error):
-					print(error)	// TODO: - Error Handling 필요
+	func didSearch(query: String) -> Observable<[AppInfo]> {
+		return Observable.create { emitter in
+
+			let requestModel = SearchAppListUseCaseRequestModel(query: query)
+			self.searchAppLoadTask = self.searchAppListUseCase.execute(requestModel: requestModel) { [weak self] (result: Result<AppInfoList, Error>) in
+				switch result {
+					case .success(let model):
+//						self?.appInfoListObservable = Observable.of(model.displayedApps)
+						emitter.onNext(model.displayedApps)
+						emitter.onCompleted()
+					case .failure(let error):
+						print(error)	// TODO: - Error Handling 필요
+				}
+			}
+
+			return Disposables.create {
+				self.searchAppLoadTask?.cancel()
 			}
 		}
+//		let requestModel = SearchAppListUseCaseRequestModel(query: query)
+//		searchAppLoadTask = searchAppListUseCase.execute(requestModel: requestModel) { [weak self] (result: Result<AppInfoList, Error>) in
+//			switch result {
+//				case .success(let model):
+//					self?.appInfoListObservable = Observable.of(model.displayedApps)
+//				case .failure(let error):
+//					print(error)	// TODO: - Error Handling 필요
+//			}
+//		}
 	}
 }
