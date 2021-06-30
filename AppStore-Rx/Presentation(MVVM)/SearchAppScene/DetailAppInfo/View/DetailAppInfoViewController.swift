77526dc9e8e3e521d2ff880fdc051355d14e2108
이종호 <bazinga940@gmail.com>
2021-06-30 12:23:40 +0900

    앱 상세 화면 파일 생성

---
+++ AppStore-Rx/Presentation(MVVM)/SearchAppScene/DetailAppInfo/View/DetailAppInfoViewController.swift
@@ -0,0 +1,15 @@
+//
+//  DetailAppInfoViewController.swift
+//  AppStore-Rx
+//
+//  Created by Jongho Lee on 2021/06/30.
+//
+
+import UIKit
+
+class DetailAppInfoViewController: UIViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+	}
+}
