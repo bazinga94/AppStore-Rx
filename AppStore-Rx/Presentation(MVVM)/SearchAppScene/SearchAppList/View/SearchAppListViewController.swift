755c300796bf4da77cb399c68bbbeda6f4e41c02
이종호 <bazinga940@gmail.com>
2021-07-28 09:16:54 +0900

    Then 라이브러리 적용(with Swift Package Manager)
    아주 좋네... UI code로 짤때 쓰면 좋을듯
    UIActivityIndicatorView Then 사용해서 구현

--- AppStore-Rx/Presentation(MVVM)/SearchAppScene/SearchAppList/View/SearchAppListViewController.swift
+++ AppStore-Rx/Presentation(MVVM)/SearchAppScene/SearchAppList/View/SearchAppListViewController.swift
@@ -9,6 +9,7 @@ import UIKit
 import RxSwift
 import RxCocoa
 import Kingfisher
+import Then
 
 class SearchAppListViewController: UIViewController, StoryboardInstantiable {
 
@@ -16,15 +17,22 @@ class SearchAppListViewController: UIViewController, StoryboardInstantiable {
 	@IBOutlet weak var tableView: UITableView!
 	@IBOutlet weak var imageView: UIImageView!
 
-	lazy var activityIndicator: UIActivityIndicatorView = {
-		let activityIndicator = UIActivityIndicatorView()
-		activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
-		activityIndicator.center = self.view.center
-		activityIndicator.hidesWhenStopped = false
-		activityIndicator.style = .medium
+//	lazy var activityIndicator: UIActivityIndicatorView = {
+//		let activityIndicator = UIActivityIndicatorView()
+//		activityIndicator.frame = CGRect(x: 0, y: 0, width: 50, height: 50)
+//		activityIndicator.center = self.view.center
+//		activityIndicator.hidesWhenStopped = false
+//		activityIndicator.style = .medium
+//
+//		return activityIndicator
+//	}()	// then 라이브러리를 사용해보자~
 
-		return activityIndicator
-	}()	// then 라이브러리를 사용해보자
+	lazy var activityIndicator = UIActivityIndicatorView().then {
+		$0.frame =  CGRect(x: 0, y: 0, width: 50, height: 50)
+		$0.center = self.view.center
+		$0.hidesWhenStopped = false
+		$0.style = .medium
+	}
 
 	private var viewModel: SearchAppListViewModelProtocol!
 	private var bag = DisposeBag()
